de7072e7dc7eba5e2989d26b58271768113e91f9
yezhang4528 <yezhang4528@gmail.com>
2019-02-16 23:45:45 -0500

    Buy sell stock done

---
+++ leetcode/problem121_140/buy_sell_stock_121/buySellStock.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1)
+            return 0;
+
+        int maxCur = 0, maxRet = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            maxCur = max(0, maxCur += (prices[i] - prices[i-1]));
+            maxRet = max(maxCur, maxRet);
+        }
+        return maxRet;
+
+    }
+};
+
+int main()
+{
+#if 0
+    vector<int> inPrice = {7,1,5,3,6,4};
+    vector<int> inPrice = {7,6,4,3,1};
+#endif
+    vector<int> inPrice = {7,6,4,3,1};
+    Solution s;
+
+    int maxReturn = s.maxProfit(inPrice);
+    cout << "Max return: " << maxReturn << endl;
+    return 0;
+}
