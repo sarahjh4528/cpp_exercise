888bd71f79d44088b6f6e04fe3a1d04813f5ff60
yezhang4528 <yezhang4528@gmail.com>
2019-03-03 18:02:00 -0500

    contain duplicate 2

---
+++ leetcode/problem201_220/contain_dup2_219/containDup2_slow.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+// 2596ms
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if (nums.empty())
+            return false;
+    
+        for (int i = 0; i < nums.size(); ++i) {
+            int loop = 1;
+            while (loop <= k && (i+loop) < nums.size()) {
+                if (nums[i] == nums[i+loop])
+                    return true;
+                ++loop;
+            }
+        }
+        return false;
+    }
+};
+int main()
+{
+#if 0
+    vector<int> nums = {1,2,3,1};
+    int k = 3;
+
+    vector<int> nums = {1,0,1,1};
+    int k = 1;
+
+    vector<int> nums = {1,2,3,1,2,3};
+    int k = 2;
+
+    vector<int> nums = {99,99};
+    int k = 2;
+
+    vector<int> nums = {};
+    int k = 0;
+
+    vector<int> nums = {1,2,3,4,5,6,7,8,9,9};
+    int k = 3;
+#endif
+    vector<int> nums = {1,2,3,1};
+    int k = 3;
+
+    Solution s;
+    bool dupk = s.containsNearbyDuplicate(nums, k);
+    cout << "Contains nearby dup : " << dupk << endl;
+    return 0;
+}
